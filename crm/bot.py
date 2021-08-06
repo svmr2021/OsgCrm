300040e5ceb70a12aafd3094e71da0c5d48cfc2f
svmr2021 <saidamir2020t@gmail.com>
2021-08-06 17:01:55 +0500

    details

--- crm/bot.py
+++ crm/bot.py
@@ -17,10 +17,20 @@ def send_message(attendance):
             text = f'{user.full_name} начал рабочий день.\nДата : {attendance.date}\nВремя прибытия : {attendance.time_in} '
             client.send_message(channel_id,text)
         elif attendance.finished:
-            text = f'{user.full_name} закончил рабочий день.\nДата : {attendance.date}\nВремя ухода : {attendance.time_out} '
-            client.send_message(channel_id, text)
+            text = f'{user.full_name} закончил рабочий день.\nДата : {attendance.date}\nВремя ухода : {attendance.time_out}\n\n' \
+                   f'Какая работа была сегодня проделана? - {attendance.standup.answer} \n'
+            client.send_message(channel_id, text,parse_mode='html')
 
 
 def send_standup(standup):
-    text = f'Опрос :\n\n{standup.attendance.user.full_name}\n{standup.q1} '
-    client.send_message(channel_id,text)
+    late = 'Причина опоздания'
+    reason = 'Не опоздал'
+    if standup.attendance.is_late:
+        reason = f'{standup.reason}'
+    text = f'{standup.attendance.user.full_name}\n' \
+           f'<b>Опрос</b>:\n' \
+           f'<b>{late}</b>:{reason}\n' \
+           f'<b>Как Ваше самочувствие?</b> - {standup.q1}\n' \
+           f'<b>Над чем вы сегодня будете работать?</b> - {standup.q1}\n' \
+           f'<b>Есть ли факторы,которые могут блокировать рабочий прогресс?</b> - {standup.q3}'
+    client.send_message(channel_id,text,parse_mode='html')
