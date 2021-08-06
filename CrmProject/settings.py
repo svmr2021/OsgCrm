bb50a1a301f23b2aeef119f6b50d0ca2305215ef
svmr2021 <saidamir2020t@gmail.com>
2021-08-06 17:47:49 +0500

    env

--- CrmProject/settings.py
+++ CrmProject/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from django.urls import reverse_lazy
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
